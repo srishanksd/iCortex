2a0c986b836b59de215974d02a2dd1b794dbfb74
srishanksd <srishanksd@gmail.com>
2026-08-15 02:52:10 +0530

    Remove file extension from User model import in Clerk webhook handler

--- backend/src/webhooks/clerk.webhook.js
+++ backend/src/webhooks/clerk.webhook.js
@@ -1,5 +1,5 @@
 import express from "express";
-import User from "../models/user.model.js";
+import User from "../models/user.model";
 import { verifyWebhook } from "@clerk/backend/webhooks";
 
 const router = express.Router();
