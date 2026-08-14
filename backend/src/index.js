4d827dc121beef00256b652314f7fad08cb049f9
srishanksd <srishanksd@gmail.com>
2026-08-14 22:16:27 +0530

    Integrate Clerk authentication into frontend application

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const FRONTEND_URL = process.env.FRONTEND_URL
 
 app.use(express.json())
 app.use(cors({origin : FRONTEND_URL, credentials : true}))
-app.use(clerkMiddleware)
+app.use(clerkMiddleware())
 
 
 app.get("/health",(req, res)=>{
